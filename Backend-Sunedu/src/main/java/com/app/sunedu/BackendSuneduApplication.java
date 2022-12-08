9b7181c5a82983aa7f15851b2b4331bd19bd2ef6
Orlando Gil <77111097+orlando2400@users.noreply.github.com>
2022-12-08 15:29:53 -0500

    Se agrega la carpeta backend del proyecto

---
+++ Backend-Sunedu/src/main/java/com/app/sunedu/BackendSuneduApplication.java
@@ -0,0 +1,13 @@
+package com.app.sunedu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendSuneduApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackendSuneduApplication.class, args);
+	}
+
+}
---
+++ Backend-Sunedu/src/test/java/com/app/sunedu/BackendSuneduApplicationTests.java
@@ -0,0 +1,13 @@
+package com.app.sunedu;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendSuneduApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
